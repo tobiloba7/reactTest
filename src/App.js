d9f51ea55ac9eef227ede92f3ae6e79fd7ac1b9c
tobiloba <ojewaleoluwatobi2@gmail.com>
2022-10-20 22:01:16 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 // import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1>"Adopt Me!"</h1>
+      {/* <h1>"Adopt Me!"</h1>
       <Pet name="white" animal="cat" breed="persian" />
       <Pet name="sadaus" animal="dog" breed="mastiff" />
-      <Pet name="flaus" animal="bird" breed="parrot" />
+      <Pet name="flaus" animal="bird" breed="parrot" /> */}
+      <h1>"Adopt Me!"</h1>
+      <SearchParams />
     </div>
   );
 };
 
 render(<App />, document.getElementById("root"));
-
-/* 
-// without JSX  
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  ]);
-};
-
-render(React.createElement(App), document.getElementById("root"));
-*/
